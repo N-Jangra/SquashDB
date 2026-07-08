a684fc7c220d3b4a80ee70b0d21ddeaf293504a8
N-Jangra <itznitinjangra@gmail.com>
2026-07-08 23:18:03 +0530

    feat: add Android native project (Capacitor, activity-alias icon/name switching)

---
+++ android/app/src/main/java/com/squashdb/tracker/MainActivity.java
@@ -0,0 +1,13 @@
+package com.squashdb.tracker;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(AppIconPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
